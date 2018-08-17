a208c71acb6edf8900bb461c13176f77a54b212f
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-08-17 15:11:58 +0800

    Update stack_split.py

--- numpy_study/stack_split.py
+++ numpy_study/stack_split.py
@@ -3,7 +3,7 @@ import numpy as np
 
 a = np.arange(11,20).reshape(3,3)
 b = a +10
-print("b:",a)
+print("b:",b)
 # 垂直合并
 # c = np.vstack((a,b))
 # 或者
@@ -14,14 +14,14 @@ print("c:",c)
 # 或者
 d,e,f = np.split(c,3,axis=0)
 print(d,e,f,sep="\n")
-
+print('---------------')
 # 水平合并
 # c2 = np.hstack((a,b))
 # 或者
-c = np.concatenate((a,b),axis=1)
-print("c:",c)
+c2 = np.concatenate((a,b),axis=1)
+print("c2:",c2)
 # 垂直拆分
 # d2,e2,f2 = np.hsplit(c,3)
 # 或者
 d2,e2,f2 = np.split(c,3,axis=1)
-print(d,e,f,sep="\n")
+print(d2,e2,f2,sep="\n")

08fa5f6858e77d1c821ffea941596fdee0cd8db4
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-08-21 15:20:50 +0800

    Update _08std.py

--- Numpy_function/_08std.py
+++ Numpy_function/_08std.py
@@ -6,12 +6,12 @@ closing_prices = np.loadtxt('data/aapl.csv',delimiter=',',usecols=(6),unpack=Tru
 mean = np.mean(closing_prices)
 devs = closing_prices - mean
 pvar = (devs ** 2).mean()
-pstd = np.sqrt(pvar)
+pstd = np.sqrt(pvar)  # 总体方差
 print(pstd)
-pstd = np.std(closing_prices)
+pstd = np.std(closing_prices) # 总体标准差
 print(pstd)
-svar = (devs ** 2).sum() / (devs.size - 1)
-sstd = np.sqrt(svar)
+svar = (devs ** 2).sum() / (devs.size - 1)#样本方差
+sstd = np.sqrt(svar)  #样本标准差
 print(sstd)
 sstd = np.std(closing_prices, ddof=1)
 print(sstd)

09398b92deb2660f83710d7ea6a327005130dcfb
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-08-21 11:08:30 +0800

    Update and rename _03.py to _03mean.py

---
+++ Numpy_function/_03mean.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+closing_prices = np.loadtxt('data/aapl.csv',delimiter=',',usecols=(6),unpack=True)
+mean = 0
+for closing_price in closing_prices:
+  mean += closing_price
+mean /= closing_prices.size
+print(mean)
+mean = np.mean(closing_prices)
+print(mean)

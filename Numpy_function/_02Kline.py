956ebdab8cb2f9f60f5fbcf0428e87173444d8be
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-08-21 11:03:31 +0800

    Update _02Kline.py

--- Numpy_function/_02Kline.py
+++ Numpy_function/_02Kline.py
@@ -20,6 +20,7 @@ dates, opening_prices, highest_prices, \
         converters={1: dmy2ymd})
 mp.figure('Candlestick', facecolor='lightgray')
 mp.title('Candlestick', fontsize=20)
+# x轴说明
 mp.xlabel('Date', fontsize=14)
 mp.ylabel('Price', fontsize=14)
 ax = mp.gca()
@@ -27,20 +28,27 @@ ax.xaxis.set_major_locator(
     md.WeekdayLocator(byweekday=md.MO))
 ax.xaxis.set_minor_locator(
     md.DayLocator())
+# 坐标轴上的文本字符串
 ax.xaxis.set_major_formatter(
     md.DateFormatter('%d %b %Y'))
 mp.tick_params(labelsize=10)
 mp.grid(linestyle=':')
 dates = dates.astype(md.datetime.datetime)
+# 关系表达式,布尔型数组,掩码数组(可用来做筛选)
 rise = closing_prices - opening_prices >= 0.01
 fall = opening_prices - closing_prices >= 0.01
+# 填充颜色
 fc = np.zeros(dates.size, dtype='3f4')
+# 边缘色
 ec = np.zeros(dates.size, dtype='3f4')
 fc[rise], fc[fall] = (1, 1, 1), (0, 0.5, 0)
 ec[rise], ec[fall] = (1, 0, 0), (0, 0.5, 0)
+# 先画影线
 mp.bar(dates, highest_prices - lowest_prices, 0,
        lowest_prices, color=fc, edgecolor=ec)
+# 实体线
 mp.bar(dates, closing_prices - opening_prices, 0.8,
        opening_prices, color=fc, edgecolor=ec)
+# 自动对水平轴字符串格式化,使较长的横坐标字符串不重叠
 mp.gcf().autofmt_xdate()
 mp.show()

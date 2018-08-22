f196a8bf0dccc340463a23ee8f372b052c834110
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-08-22 09:27:24 +0800

    Update _14bollinger_bands.py

--- Numpy_function/_14bollinger_bands.py
+++ Numpy_function/_14bollinger_bands.py
@@ -1,1 +1,15 @@
+import matplotlib.pyplot as mp
+import matplotlib.dates as md
 
+
+def dmy2ymd(dmy):
+    dmy = str(dmy, encoding='utf-8')
+    date = dt.datetime.strptime(
+        dmy, '%d-%m-%Y').date()
+    ymd = date.strftime('%Y-%m-%d')
+    return ymd
+dates, closing_prices = np.loadtxt(
+    'data/aapl.csv', delimiter=',',
+    usecols=(1, 6), unpack=True,
+    dtype=np.dtype('M8[D], f8'),
+    converters={1: dmy2ymd})

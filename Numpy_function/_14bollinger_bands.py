e8b7faebc8c4b687c5e0d368348e3d4dc5e53020
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-08-22 09:32:32 +0800

    Update _14bollinger_bands.py

--- Numpy_function/_14bollinger_bands.py
+++ Numpy_function/_14bollinger_bands.py
@@ -17,3 +17,18 @@ mp.figure('Moving Average', facecolor='lightgray')
 mp.title('Moving Average', fontsize=20)
 mp.xlabel('Date', fontsize=14)
 mp.ylabel('Price', fontsize=14)
+ax = mp.gca()
+ax.xaxis.set_major_locator(
+    md.WeekdayLocator(byweekday=md.MO))
+ax.xaxis.set_minor_locator(
+    md.DayLocator())
+ax.xaxis.set_major_formatter(
+    md.DateFormatter('%d %b %Y'))
+mp.tick_params(labelsize=10)
+mp.grid(linestyle=':')
+dates = dates.astype(md.datetime.datetime)
+mp.plot(dates, closing_prices, c='lightgray',
+        label='Closing Price')
+mp.legend()
+mp.gcf().autofmt_xdate()
+mp.show()

d173caf53f9295b04e4ac83b85055dbc0738fbd1
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-08-24 16:43:16 +0800

    Create _13det.py

---
+++ numpy_ufunc/_13det.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+A = np.mat('2 1; 3 4')
+print(A)
+a = np.linalg.det(A)
+print(a)
+B = np.mat('3 2 1; 4 9 8; 5 6 7')
+print(B)
+b = np.linalg.det(B)
+print(b)

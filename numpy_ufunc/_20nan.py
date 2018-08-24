ef9941546dded08b7f10043d8a9bac02fed03704
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-08-24 16:49:19 +0800

    Create _20nan.py

---
+++ numpy_ufunc/_20nan.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+a = np.array([1, 2, np.nan, 3, 4])
+print(np.max(a), np.min(a))
+print(np.argmax(a), np.argmin(a))
+print(np.nanmax(a), np.nanmin(a))
+print(np.nanargmax(a), np.nanargmin(a))

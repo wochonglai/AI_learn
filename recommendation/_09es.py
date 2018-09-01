f43afa4515a28469e3dd9a24e21ace14fadd8f29
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-01 08:50:39 +0800

    Create _09es.py
    欧氏距离

---
+++ recommendation/_09es.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+'''
+欧式距离
+'''
+import json
+import numpy as np
+with open('data/ratings.json', 'r') as f:
+    ratings = json.loads(f.read())
+users, scmat = list(ratings.keys()), []
+for user1 in users:
+    scrow = []
+    for user2 in users:
+        movies = set()  # 存放两个人共同看的
+        for movie in ratings[user1]:
+            if movie in ratings[user2]:
+                movies.add(movie)
+        if len(movies) == 0:
+            score = 0
+        else:
+            x, y = [], []
+            for movie in movies:
+                x.append(ratings[user1][movie])
+                y.append(ratings[user2][movie])
+            x = np.array(x)
+            y = np.array(y)
+            score = 1 / (1 + np.sqrt(((x - y) ** 2).sum()))
+        scrow.append(score)
+    scmat.append(scrow)
+users = np.array(users)
+scmat = np.array(scmat)
+for user in users:
+    print('"%s"'%user,end='  ')
+print()
+for scrow in scmat:
+    print(' '.join('{:>5.2f}'.format(score) for score in scrow))

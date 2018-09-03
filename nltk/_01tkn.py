8aae55c7c93f92bb465dcb98847747359cbbcea5
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-03 09:50:04 +0800

    Update _01tkn.py

--- nltk/_01tkn.py
+++ nltk/_01tkn.py
@@ -1,1 +1,19 @@
+# -*- coding: utf-8 -*-
+import nltk.tokenize as tk
 
+
+doc = "Are you curious about tokenization? " \
+      "Let's see how it works! " \
+      "We need to analyze a couple of sentences " \
+      "with punctuations to see it in action."
+print(doc)
+tokens = tk.sent_tokenize(doc)
+for i, token in enumerate(tokens):
+    print(i + 1, token)
+tokens = tk.word_tokenize(doc)
+for i, token in enumerate(tokens):
+    print(i + 1, token)
+tokenizer = tk.WordPunctTokenizer()
+tokens = tokenizer.tokenize(doc)
+for i, token in enumerate(tokens):
+    print(i + 1, token)

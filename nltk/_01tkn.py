38605b91abcb1b0fd06c7de7e93ca6f6c80706b4
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-03 10:03:08 +0800

    Update _01tkn.py
    分词

--- nltk/_01tkn.py
+++ nltk/_01tkn.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import nltk.tokenize as tk
+import nltk.tokenize as tk  #分词包
 
 
 doc = "Are you curious about tokenization? " \
@@ -7,12 +7,18 @@ doc = "Are you curious about tokenization? " \
       "We need to analyze a couple of sentences " \
       "with punctuations to see it in action."
 print(doc)
+# 基于句子分词
 tokens = tk.sent_tokenize(doc)
 for i, token in enumerate(tokens):
     print(i + 1, token)
+print(1,'+'*19)
+# 基于单词的拆分
 tokens = tk.word_tokenize(doc)
 for i, token in enumerate(tokens):
     print(i + 1, token)
+print(2,'+'*19)
+
+# 单词标点分词器对象,会以所有标点拆分
 tokenizer = tk.WordPunctTokenizer()
 tokens = tokenizer.tokenize(doc)
 for i, token in enumerate(tokens):

23dde85a50d4abdd42f2ee2010828adb063c2d37
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-04 09:07:37 +0800

    Update _03lemmatize.py
    词型还原

--- nltk/_03lemmatize.py
+++ nltk/_03lemmatize.py
@@ -5,3 +5,8 @@ words = ['table', 'probably', 'wolves', 'playing', 'is',
          'dog', 'the', 'beaches', 'grounded', 'dreamt',
          'envision']
 # 词形还原器
+lmm = ns.WordNetLemmatizer()
+for word in words:
+    n = lmm.lemmatize(word, pos='n')  # 名词
+    v = lmm.lemmatize(word, pos='v')  # 动词
+    print('{:10} {:10} {:10}'.format(word, n, v))

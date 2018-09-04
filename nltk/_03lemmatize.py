fe12b3de2fe2d27dcb8376047d67c93119693236
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-04 09:07:06 +0800

    Create _03lemmatize.py

---
+++ nltk/_03lemmatize.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+'''词型还原'''
+import nltk.stem as ns
+words = ['table', 'probably', 'wolves', 'playing', 'is',
+         'dog', 'the', 'beaches', 'grounded', 'dreamt',
+         'envision']
+# 词形还原器

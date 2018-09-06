e0a43fadff7da14be82598f936fbce2184444586
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-06 10:03:48 +0800

    Update _05tfidf.py
    词频逆文档频率(TF-IDF)

--- nltk/_05tfidf.py
+++ nltk/_05tfidf.py
@@ -1,1 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+词频逆文档频率(TF-IDF)
+'''
+import nltk.tokenize as tk
+import sklearn.feature_extraction.text as ft
+import sklearn.preprocessing as sp
+doc = 'The brown dog is running. ' \
+      'The black dog is in the black room. ' \
+      'Running in the room is forbidden.'
+print(doc)
+sentences = tk.sent_tokenize(doc)
+for i, sentence in enumerate(sentences):
+    print(i + 1, sentence)
+cv = ft.CountVectorizer() # 统计词袋用
+bow = cv.fit_transform(sentences).toarray() #词袋矩阵
+print(bow)
+words = cv.get_feature_names()  # 文档词典,默认以首字母排序
+print(words)
 
+# 词频
+tf = sp.normalize(bow, norm='l1')
+print(tf)
+
+# 词频逆文档转换器
+tt = ft.TfidfTransformer()
+tfidf = tt.fit_transform(bow).toarray()
+print(tfidf)

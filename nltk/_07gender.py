03a15e26bc5fea8db45834e271be96fa29af3c01
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-06 10:06:33 +0800

    Update _07gender.py

--- nltk/_07gender.py
+++ nltk/_07gender.py
@@ -1,1 +1,11 @@
-
+# -*- coding: utf-8 -*-
+'''
+性别识别
+'''
+import random
+import numpy as np
+import nltk.corpus as nc
+import nltk.classify as cf
+male_names = nc.names.words('male.txt')
+female_names = nc.names.words('female.txt')
+models, acs = [], []

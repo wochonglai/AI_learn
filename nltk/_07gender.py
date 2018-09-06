7977d37b9663a59bf08324e5693b86271d23002b
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-06 10:06:55 +0800

    Update _07gender.py

--- nltk/_07gender.py
+++ nltk/_07gender.py
@@ -9,3 +9,14 @@ import nltk.classify as cf
 male_names = nc.names.words('male.txt')
 female_names = nc.names.words('female.txt')
 models, acs = [], []
+for n_letters in range(1, 6):
+    data = []
+    for male_name in male_names:
+        feature = {
+            'feature': male_name[-n_letters:].lower()}  # 截取名字的后几个字母
+        data.append((feature, 'male'))
+    for female_name in female_names:
+        feature = {
+            'feature': female_name[-n_letters:].lower()}
+        data.append((feature, 'female'))
+    random.seed(7)

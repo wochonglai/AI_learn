873029e29cc28c688e343786e32017e29e3c52ac
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-07 11:04:58 +0800

    Update _09topic.py

--- nltk/_09topic.py
+++ nltk/_09topic.py
@@ -10,3 +10,10 @@ import nltk.stem.snowball as sb
 import gensim.models.ldamodel as gm
 import gensim.corpora as gc
 doc = []
+with open('../data2/topic.txt', 'r') as f:
+    for line in f.readlines():
+        doc.append(line[:-1])
+tokenizer = tk.RegexpTokenizer(r'\w+')  # 基于正则表达式分词
+stopwords = nc.stopwords.words('english')   # 去除非主要词
+stemmer = sb.SnowballStemmer('english')     # 词干提取器
+lines_tokens = []

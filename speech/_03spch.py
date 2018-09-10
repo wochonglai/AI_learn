55c7ee03ef138363197a0221b23b0e03968901be
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-10 20:34:38 +0800

    Update _03spch.py

--- speech/_03spch.py
+++ speech/_03spch.py
@@ -1,1 +1,14 @@
-
+# -*- coding: utf-8 -*-
+'''
+隐马尔科夫模型
+'''
+import os
+import warnings
+import numpy as np
+import scipy.io.wavfile as wf
+import python_speech_features as sf
+# pip install C:\Users\Python\Downloads\hmmlearn-0.2.1-cp36-cp36m-win_amd64.whl
+import hmmlearn.hmm as hl   # 隐马尔科夫模型
+warnings.filterwarnings('ignore',
+                        category=DeprecationWarning)
+np.seterr(all='ignore')

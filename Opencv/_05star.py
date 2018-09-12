68d626e93de803d7e105ec0fd9cde5bf86c34d15
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-12 10:31:07 +0800

    Update _05star.py
    star特征检测

--- Opencv/_05star.py
+++ Opencv/_05star.py
@@ -7,3 +7,11 @@ original = cv.imread('../data2/table.jpg')
 cv.imshow('Original', original)
 gray = cv.cvtColor(original, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
+# opencv-contrib-python
+star = cv.xfeatures2d.StarDetector_create()
+keypoints = star.detect(gray)
+mixture = original.copy()
+cv.drawKeypoints(original, keypoints, mixture,
+                 flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv.imshow('Mixture', mixture)
+cv.waitKey()

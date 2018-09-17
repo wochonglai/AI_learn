7499ae298b59f6f0a4e606d1262fd0b6610e606d
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-17 22:44:49 +0800

    Update _06.py

--- Opencv/_06.py
+++ Opencv/_06.py
@@ -2,3 +2,9 @@
 '''
 sift特征检测
 '''
+import cv2 as cv
+original = cv.imread('../data2/table.jpg')
+cv.imshow('Original', original)
+gray = cv.cvtColor(original, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+sift = cv.xfeatures2d.SIFT_create()

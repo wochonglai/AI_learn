e7b32ba6e75fb4cf036feb01b41c0c608ae4ad70
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-19 17:33:10 +0800

    Update _07.py

--- Opencv/_07.py
+++ Opencv/_07.py
@@ -2,3 +2,10 @@
 '''
 特征(描述)矩阵
 '''
+import cv2 as cv
+import matplotlib.pyplot as mp
+original = cv.imread('../data2/table.jpg')
+cv.imshow('Original', original)
+gray = cv.cvtColor(original, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+star = cv.xfeatures2d.StarDetector_create()

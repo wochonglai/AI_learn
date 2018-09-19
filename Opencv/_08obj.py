0126c5aebba1323c7f5d99c5a8ab33b3c962c00a
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-19 17:37:19 +0800

    Update _08obj.py

--- Opencv/_08obj.py
+++ Opencv/_08obj.py
@@ -12,18 +12,28 @@ warnings.filterwarnings('ignore',
 np.seterr(all='ignore')
 
 
-def search_objects(directory):
-    directory = os.path.normpath(directory)
-    if not os.path.isdir(directory):
-        raise IOError("The directory '" + directory +
-                      "' doesn't exist!")
-    objects = {}
-    for curdir, subdirs, files in os.walk(directory):
-        for jpeg in (file for file in files
-                     if file.endswith('.jpg')):
-            path = os.path.join(curdir, jpeg)
-            label = path.split(os.path.sep)[-2]
-            if label not in objects:
-                objects[label] = []
-            objects[label].append(path)
-    return objects
+train_objects = search_objects(
+    '../data2/objects/training')
+train_x, train_y = [], []
+for label, filenames in train_objects.items():
+    descs = np.array([])
+    for filename in filenames:
+        image = cv.imread(filename)
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        # 取图像的高和宽
+        h, w = gray.shape[:2]
+        # 放大倍数
+        f = 200 / min(h, w)
+        gray = cv.resize(gray, None, fx=f, fy=f)
+        star = cv.xfeatures2d.StarDetector_create()
+        # 取图像的关键点
+        keypoints = star.detect(gray)
+        sift = cv.xfeatures2d.SIFT_create()
+        _, desc = sift.compute(gray, keypoints)
+        if len(descs) == 0:
+            descs = desc
+        else:
+            descs = np.append(descs, desc, axis=0)  # axis=0按行扩展
+    train_x.append(descs)
+    train_y.append(label)
+models = {}

010e6c97047411d9a9319c5887152be15abd928d
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-20 22:46:50 +0800

    Update _09.py

--- Opencv/_09.py
+++ Opencv/_09.py
@@ -5,3 +5,11 @@
 import numpy as np
 import cv2 as cv
 vc = cv.VideoCapture(0)  # 0 - 视频捕捉设备编号
+vc = cv.VideoCapture(1)  # 1 - 视频捕捉设备编号
+while True:
+    frame = vc.read()[1]
+    cv.imshow('VideoCapture', frame)
+    if cv.waitKey(33) == 27:  # 30 fps,延时33ms,一秒内30幅画面,按键返回
+        break
+vc.release()    # vc资源释放
+cv.destroyAllWindows()  # 高版本opencv可不调用

9154d3c8fa931de4436db09107feaf3566d84ea8
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-23 08:49:33 +0800

    Update _11.py

--- Opencv/_11.py
+++ Opencv/_11.py
@@ -1,3 +1,22 @@
+
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+%matplotlib inline
+ 
 import matplotlib.pyplot as plt
 import csv
 from textblob import TextBlob
@@ -8,5 +27,9 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC, LinearSVC
-from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matr
+from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matrix
+from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import StratifiedKFold, cross_val_score, train_test_split 
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.learning_curve import learning_curve

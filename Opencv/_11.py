c2d44d25147d857d01f527e9d2e140f39bf37523
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-23 08:48:32 +0800

    Update _11.py

--- Opencv/_11.py
+++ Opencv/_11.py
@@ -2,3 +2,10 @@ import matplotlib.pyplot as plt
 import csv
 from textblob import TextBlob
 import pandas
+import sklearn
+import cPickle
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC, LinearSVC
+from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matr

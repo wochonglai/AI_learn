1317cc2b032e581e8c998180448409af72a65728
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-23 08:48:49 +0800

    Update _11.py

--- Opencv/_11.py
+++ Opencv/_11.py
@@ -9,3 +9,4 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC, LinearSVC
 from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matr
+from sklearn.grid_search import GridSearchCV

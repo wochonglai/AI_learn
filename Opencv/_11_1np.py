fae1a6b3519cc78c5a5d7cb6c7c29873b9884d46
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-25 21:15:11 +0800

    Update _11_1np.py

--- Opencv/_11_1np.py
+++ Opencv/_11_1np.py
@@ -1,1 +1,10 @@
-# ceshi
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import numpy as np
+# 原始样本
+A = np.mat('3 2000; 2 3000; 4 5000; 5 8000; 1 2000',
+           dtype=float)
+print('A =', A, sep='\n')
+# 均值为0，极差为1
+mu = A.mean(axis=0)
+s = A.max(axis=0) - A.min(axis=0)

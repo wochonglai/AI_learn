e28ebd088a0b1a2147ab3e7072e388ee7f6207b3
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-25 21:17:01 +0800

    Update _12_2sk.py

--- Opencv/_12_2sk.py
+++ Opencv/_12_2sk.py
@@ -3,3 +3,7 @@ import numpy as np
 import sklearn.pipeline as pl
 import sklearn.preprocessing as sp
 import sklearn.decomposition as dc
+# 原始样本
+A = np.mat('3 2000; 2 3000; 4 5000; 5 8000; 1 2000',
+           dtype=float)
+print('A =', A, sep='\n')

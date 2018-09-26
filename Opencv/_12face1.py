14cb1a644c6c1f1d794c2d8dadf9be2a88d8a675
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-26 22:38:26 +0800

    Update _12face1.py

--- Opencv/_12face1.py
+++ Opencv/_12face1.py
@@ -3,3 +3,7 @@ import matplotlib.pyplot as mp
 faces = sd.fetch_olivetti_faces('../../data/')
 x = faces.data
 y = faces.target
+mp.figure('Olivetti Faces', facecolor='black')
+mp.subplots_adjust(left=0.04, bottom=0.04,
+	right=0.96, top=0.96, wspace=0, hspace=0)
+rows, cols = 10, 40

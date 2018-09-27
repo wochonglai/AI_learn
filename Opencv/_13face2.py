05bf4e746d55f5190dda3689e2320b1e01b74d31
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-28 01:12:37 +0800

    Update _13face2.py

--- Opencv/_13face2.py
+++ Opencv/_13face2.py
@@ -1,3 +1,6 @@
 import sklearn.datasets as sd
 import sklearn.decomposition as dc
 import sklearn.model_selection as ms
+import sklearn.svm as svm
+import sklearn.metrics as sm
+import matplotlib.pyplot as mp

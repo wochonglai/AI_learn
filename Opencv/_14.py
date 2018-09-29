eb286df0f1df9a45792d040fe95030d6b9f63ff8
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-09-29 19:54:36 +0800

    Update _14.py

--- Opencv/_14.py
+++ Opencv/_14.py
@@ -3,3 +3,7 @@ import numpy as np
 A = np.mat(
 	'3 2000; 2 3000; 4 5000; 5 8000; 1 2000',
 	dtype=float)
+print('A =', A, sep='\n')
+# 归一化缩放：均值为0，极差为1
+mu = A.mean(axis=0)
+s = A.max(axis=0) - A.min(axis=0)

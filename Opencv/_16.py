1d96877d39a9ed2d485da413b2b5f43e576d54ae
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-10-01 06:46:50 +0800

    Update _16.py

--- Opencv/_16.py
+++ Opencv/_16.py
@@ -1,3 +1,8 @@
 import sklearn.datasets as sd
 import sklearn.decomposition as dc
 import matplotlib.pyplot as mp
+faces = sd.fetch_olivetti_faces('../../data/')
+x = faces.data
+y = faces.target
+ncps = range(10, 410, 10)
+evrs = []

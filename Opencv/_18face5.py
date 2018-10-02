c8b00563f133846d6b10ff8b3f208a729d6df81f
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-10-02 13:05:00 +0800

    Create _18face5.py

---
+++ Opencv/_18face5.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import sklearn.datasets as sd
+import sklearn.decomposition as dc
+import sklearn.model_selection as ms
+import sklearn.svm as svm
+import sklearn.metrics as sm
+import matplotlib.pyplot as mp

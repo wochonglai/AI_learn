c5889b9acf3cac736e28ae99f080da523555fccb
wochonglai <39129777+wochonglai@users.noreply.github.com>
2018-10-03 09:29:54 +0800

    Update _19.py

--- Opencv/_19.py
+++ Opencv/_19.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 import sklearn.datasets as sd
 import sklearn.decomposition as dc
 import matplotlib.pyplot as mp
+x, y = sd.make_circles(n_samples=500, factor=0.2,
+                       noise=0.04)
+model = dc.KernelPCA(kernel='rbf',
+                     fit_inverse_transform=True,
+                     gamma=10)
+kpca_x = model.fit_transform(x)
